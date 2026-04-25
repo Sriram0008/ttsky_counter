3ae86ea2a275b30cff1d169dad50450f67d1ac07
SRIRAM M R <mrsriram195@gmail.com>
2026-04-25 14:37:28 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -2,20 +2,21 @@
 `timescale 1ns / 1ps
 
 module tb ();
+  initial begin
+    $dumpfile("tb.vcd");
+    $dumpvars(0, tb);
+  end
 
-  // Initial inputs
   reg [7:0] ui_in;
   reg [7:0] uio_in;
   reg       ena;
   reg       clk;
   reg       rst_n;
 
-  // Outputs
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
-  // Instantiate the Unit Under Test (UUT)
   tt_um_example uut (
     .ui_in   (ui_in),
     .uo_out  (uo_out),
@@ -26,13 +27,4 @@ module tb ();
     .clk     (clk),
     .rst_n   (rst_n)
   );
-
-  // We leave clk and rst_n for Cocotb to drive.
-  // We just need to dump the waveforms.
-  initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
-    #1;
-  end
-
 endmodule
